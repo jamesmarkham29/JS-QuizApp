0817db95f0ac2fb979ff70aa3a8c9a7bd4b3e53c
james.markham29 <james.markham29@student.tamdistrict.org>
2026-01-13 18:54:34 +0000

    Adding real questions

--- quiz.js
+++ quiz.js
@@ -1,61 +1,59 @@
-// Web app's Firebase configuration
-  var firebaseConfig = {
-    apiKey: "AIzaSyAbFistmgFQcIOojLVD7cgXmTE9mLvuqVc",
-    authDomain: "quizapp-74d37.firebaseapp.com",
-    databaseURL: "https://quizapp-74d37.firebaseio.com",
-    projectId: "quizapp-74d37",
-    storageBucket: "quizapp-74d37.appspot.com",
-    messagingSenderId: "10113678267",
-    appId: "1:10113678267:web:d2aa73777b379fb241629e",
-    measurementId: "G-PEKPZ347TY"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  var database = firebase.database();
+var getQuestion = document.getElementById("question");
+var getAnswer1 = document.getElementById("answer1");
+var getAnswer2 = document.getElementById("answer2");
+var getAnswer3 = document.getElementById("answer3");
+var getScore = document.getElementById("showScore");
 
-
-
-  var getQuestion = document.getElementById("question");
-  var getAnswer1 = document.getElementById("answer1");
-  var getAnswer2 = document.getElementById("answer2");
-  var getAnswer3 = document.getElementById("answer3");
-  var getScore = document.getElementById("showScore");
-
-
-  var quizQuestions;
-  var questionId = 0;
-  var score = 1;
-  
-// getting quiz data from Firebase
-database.ref('QuizData').on('value', function(questions){
-    quizQuestions = questions.val();
-});
-
+var quizQuestions = [
+  {
+    question: "Which planet is known as the Red Planet?",
+    answer1: "Venus",
+    answer2: "Mars",
+    answer3: "Jupiter",
+    correctAnswer: "Mars",
+  },
+  {
+    question: "What is the largest ocean on Earth?",
+    answer1: "Atlantic Ocean",
+    answer2: "Pasific Ocean",
+    answer3: "Indian Ocean",
+    correctAnswer: "Pasific Ocean",
+  },
+  {
+    question: "What gas do plants absorb during photosynthesis?",
+    answer1: "Carbon Dioxide",
+    answer2: "Oxygen",
+    answer3: "Nitrogen",
+    correctAnswer: "Carbon Dioxide",
+  },
+];
+var questionId = 0;
+var score = 1;
 
 let getQuest = () => {
   //for (question in quizQuestions) {
-    getQuestion.innerHTML = quizQuestions[questionId].question;
-    getAnswer1.innerText = quizQuestions[questionId].answer1;
-    getAnswer2.innerText = quizQuestions[questionId].answer2;
-    getAnswer3.innerText = quizQuestions[questionId].answer3;
-    getScore.innerText = score;
-    if(quizQuestions[questionId].correctAnswer)
-    {}
+  getQuestion.innerHTML = quizQuestions[questionId].question;
+  getAnswer1.innerText = quizQuestions[questionId].answer1;
+  getAnswer2.innerText = quizQuestions[questionId].answer2;
+  getAnswer3.innerText = quizQuestions[questionId].answer3;
+  getScore.innerText = score;
+  if (quizQuestions[questionId].correctAnswer) {
+  }
   //}
-}
+};
 
 setInterval(getQuest, 1000);
 
 //quizQuestions[1].correctAnswer;
-questionId = questionId + 1;
+// questionId = questionId + 1;
 
-// showing quiz data 
+// showing quiz data
 let nextQuestion = () => {
-    questionId = questionId + 1;
-    getQuestion.innerHTML = quizQuestions[questionId].question;
-    getAnswer1.innerText = quizQuestions[questionId].answer1;
-    getAnswer2.innerText = quizQuestions[questionId].answer2;
-    getAnswer3.innerText = quizQuestions[questionId].answer3;
-    console.log(quizQuestions[question].correctAnswer)
-    questionId = questionId;
-}
+  questionId = questionId + 1;
+  getQuestion.innerHTML = quizQuestions[questionId].question;
+  getAnswer1.innerText = quizQuestions[questionId].answer1;
+  getAnswer2.innerText = quizQuestions[questionId].answer2;
+  getAnswer3.innerText = quizQuestions[questionId].answer3;
+  console.log(quizQuestions[question].correctAnswer);
+  questionId = questionId;
+};
